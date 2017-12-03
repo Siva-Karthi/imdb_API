2432f059edcf4a23c46c1ab4665ba8eb3551c029
Siva-Karthi <sivasubramani.612@gmail.com>
2017-12-03 16:30:45 +0530

    search movie implemented

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 
-from api.views import MovieCreateView, MovieDetailView
+from api.views import MovieCreateView, MovieDetailView, SearchAPIView
 
 urlpatterns = [
     # url(r'^movies/$', MovieCreateView.as_view(), name='movies'),
     #url(r'^movies/(?P<id>[0-9]+)$', MovieDetailView.as_view(), name='detail'),
     # url(r'^movies/(?P<pk>[0-9]+)$', MovieDetailView.as_view(), name='detail'),
     url(r'^movies[/]*$', MovieCreateView.as_view(), name='movies'),
-    url(r'^movies/(?P<pk>[0-9]+)[/]*$', MovieDetailView.as_view(), name='detail'),
+    # url(r'^movies/(?P<pk>[0-9]+)[/]*$', MovieDetailView.as_view(), name='detail'),
+    url('^search/(?P<movie_name>\w+)/$', SearchAPIView.as_view())
 ]
 
--- api/views.py
+++ api/views.py
@@ -4,8 +4,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
-
+from rest_framework.views import APIView
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
 
 from api.models import MovieModel
 from api.serializers import MovieSerializer
@@ -21,3 +22,14 @@ class MovieDetailView(RetrieveUpdateDestroyAPIView):
     queryset = MovieModel.objects.all()
     serializer_class = MovieSerializer
     permission_classes = (IsAdminOrReadOnly, )
+
+class SearchAPIView(APIView):
+    """
+        APIView for search.Only get method is defined.
+    """
+    def get(self, request, movie_name):
+        """takes movie_name as get parameter"""
+        if movie_name:
+            movies = MovieModel.objects.filter(name__icontains=movie_name)
+            movies_serializer = MovieSerializer(movies, many=True)
+            return Response(movies_serializer.data)
