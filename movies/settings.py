9c5e82c39610356a689d3e72b7ac704b548f7535
Siva-Karthi <sivasubramani.612@gmail.com>
2017-12-05 14:28:24 +0530

    specify static root dir

--- movies/settings.py
+++ movies/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
